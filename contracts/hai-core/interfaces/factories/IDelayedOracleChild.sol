f77641b20cc28c4cd6b79e15efd7075270cbd5ad
Luke Hamilton <pies.eth@pm.me>
2023-09-27 22:09:42 -0400

    remove contract to test workflow action

--- contracts/hai-core/interfaces/factories/IDelayedOracleChild.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.19;
-
-import {IDelayedOracle} from '@interfaces/oracles/IDelayedOracle.sol';
-
-import {IFactoryChild} from '@interfaces/factories/IFactoryChild.sol';
-
-interface IDelayedOracleChild is IDelayedOracle, IFactoryChild {}
